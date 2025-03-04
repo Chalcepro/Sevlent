dacc6463daba969401ec300873ec6f5f5da9b56c
Arun chahar <info@vfixtechnology.com>
2025-03-04 17:47:48 +0530

    fixed auth notification error issue by stopping it

--- config/authentication-log.php
+++ config/authentication-log.php
@@ -19,20 +19,20 @@ return [
     'notifications' => [
         'new-device' => [
             // Send the NewDevice notification
-            'enabled' => env('NEW_DEVICE_NOTIFICATION', true),
+            'enabled' => env('NEW_DEVICE_NOTIFICATION', false),
 
             // Use torann/geoip to attempt to get a location
-            'location' => true,
+            'location' => false,
 
             // The Notification class to send
             'template' => \Rappasoft\LaravelAuthenticationLog\Notifications\NewDevice::class,
         ],
         'failed-login' => [
             // Send the FailedLogin notification
-            'enabled' => env('FAILED_LOGIN_NOTIFICATION', true),
+            'enabled' => env('FAILED_LOGIN_NOTIFICATION', false),
 
             // Use torann/geoip to attempt to get a location
-            'location' => true,
+            'location' => false,
 
             // The Notification class to send
             'template' => \Rappasoft\LaravelAuthenticationLog\Notifications\FailedLogin::class,
